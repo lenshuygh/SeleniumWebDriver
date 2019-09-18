5dfbff53639158e453242aa6c9c85d431f2b231e
lens <lens.huygh@gmail.com>
2019-09-18 22:22:12 +0200

    finished WebDriverRadioButtons.java added notes on radiobuttons selecting , choosing & checking added checkboxestest.html

--- src/main/java/be/lens/WebDriverRadioButtons.java
+++ src/main/java/be/lens/WebDriverRadioButtons.java
@@ -1,7 +1,23 @@
 package be.lens;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class WebDriverRadioButtons {
     public static void main(String[] args) {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("http://localhost:63342/WebDriverDemoSolution/src/main/webapp/RadioButtonTest.html?_ijt=bufvv7jvld0jmkueqaj4uedgdt");
+        List<WebElement> radioButtons = webDriver.findElements(By.name("color"));
+        radioButtons.get(1).click();
 
+        for (WebElement radioButton : radioButtons) {
+            if(radioButton.isSelected()){
+                System.out.println(radioButton.getAttribute("value"));
+            }
+        }
     }
 }
